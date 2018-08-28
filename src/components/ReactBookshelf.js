7a72e7d6fc76315b472da596e8d8a8d43bc4c912
Calvin B <cbissett88@gmail.com>
2018-08-28 11:37:33 -0400

    working bookShelf

--- src/components/ReactBookshelf.js
+++ src/components/ReactBookshelf.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
 class ReactBookshelf extends Component {
     render() {
         const { books } = this.props;
         let searchResults;
-        
         if ((books.map)) {
             searchResults =  books.map((book) => (
                 ((book.imageLinks) ?            
+                    <li>
+                        <div className="book">
+                            <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.thumbnail + ')' }}></div>
+                                <div className="book-shelf-changer">
+                                    <select>
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
+                        </div>
+                    </li>
+                :          
                 <li>
                     <div className="book">
                         <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + book.imageLinks.thumbnail + ')' }}></div>
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + '/src/img/blank-book-cover-template.jpg' + ')' }}></div>
                             <div className="book-shelf-changer">
                                 <select>
                                     <option value="move" disabled>Move to...</option>
@@ -26,25 +43,6 @@ class ReactBookshelf extends Component {
                         <div className="book-authors">{book.authors}</div>
                     </div>
                 </li>
-            :          
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + '/src/img/blank-book-cover-template.jpg' + ')' }}></div>
-                        <div className="book-shelf-changer">
-                            <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
-                </div>
-            </li>
             )))
         } else {
             searchResults = <p>Nothing Found</p>
